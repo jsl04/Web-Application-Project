de40d12421759e76d86ad9a0d22b7ec9a06d0678
jsl04 <jaloftis@cisco.com>
2016-04-29 14:33:37 -0400

    Final

--- server.R
+++ server.R
@@ -11,17 +11,19 @@ library(dplyr)
 library(rCharts)
 library(markdown)
 
-mpg_data<- mtcars
+mpg_data<-datasets::mtcars
 mpg_data$am[which(mpg_data$am==0)] <- "Automatic"
 mpg_data$am[which(mpg_data$am==1)] <- "Manual"
 mpg_data$Transmission_Type<- as.factor(mpg_data$am)
 mpg_data$model<- row.names(mpg_data)
 mpg_data$Power_To_Weight_Ratio<- mpg_data$hp/mpg_data$wt
+mpg_data<- mpg_data[-9]
 
 shinyServer(function(input,output){
   
-  output$mychart<- renderChart2({
-    p1<-rPlot(input$Power_to_Weight,input$mpg,data = mpg_data, type= "point", tooltip= "#!function(item){ return 'Car Model: ' + item.model + ' Power to Weight Ratio: ' + item.Power_To_Weight_Ratio + ' MPG: ' + item.mpg }!#")
+  output$myChart<- renderChart({
+    p1<-rPlot(input$y,input$x,data = mpg_data, type= "point",color=input$z, tooltip= "#!function(item){ return 'Car Model: ' + item.model + ' Power to Weight Ratio: ' + item.Power_To_Weight_Ratio + ' MPG: ' + item.mpg }!#")
+    p1$addParams(dom = 'myChart')
     return(p1)
   })
 })
--- ui.R
+++ ui.R
@@ -11,26 +11,39 @@ library(dplyr)
 library(rCharts)
 library(markdown)
 
+mpg_data<-datasets::mtcars
+mpg_data$am[which(mpg_data$am==0)] <- "Automatic"
+mpg_data$am[which(mpg_data$am==1)] <- "Manual"
+mpg_data$Transmission_Type<- as.factor(mpg_data$am)
+mpg_data$model<- row.names(mpg_data)
+mpg_data$Power_To_Weight_Ratio<- mpg_data$hp/mpg_data$wt
+mpg_data<- mpg_data[-9]
+
 shinyUI(pageWithSidebar(
-  headerPanel("Interactive chart on comparing power to weight ratio to miles per gallon"),
+  headerPanel("Interactive Graph for Motor Trend Vehicle Data featuring Power To Weight Ratio"),
   
   sidebarPanel(
-    sliderInput("mpg",
-                "Range:",
-                min=10.4,
-                max =33.9,
-                value = c(10.4,33.9),
-                sep = ""),
-    sliderInput("Power_to_Weight",
-                "Range:",
-                min = 19.43574,
-                max = 93.83754,
-                value = c(19.43574,93.83754),
-                sep = "")
+    selectInput(inputId = "x",
+                label = "Choose Car Variable",
+                choices = colnames(mpg_data),
+                selected = "Power_To_Weight_Ratio"),
+    selectInput(inputId = "y",
+                label = "Choose Car Variable to Compare with Selection Above",
+                choices = colnames(mpg_data),
+                selected = "mpg"),
+    selectInput(inputId = "z",
+                label = "Choose Car Variable to Segment Data Points by Color",
+                choices = colnames(mpg_data),
+                selected = "Transmission_Type")
   ),
   mainPanel(
-    showOutput("mychart","polycharts")
+    showOutput("myChart", "polycharts"),
+    h4 ("About this dashboard and how to use it"),
+    p("This dashboard is meant for car buyers interested in the vehicles listed in the motortrend data included with R dataset package. Use the three selectors on the side to pick out which measures you want to compare.
+      Use the first selector to choose the x axis variable, the second selector to choose the y axis variable, and the third selector to choose a variable to segement to data by color. 
+      In addition, you can hover over each data point to see the vehicle model, the power to weight ratio, and the miles per gallon ",
+      style="font-family: 'times';font-si16pt")
   )
-)
-)
+))
+
 
